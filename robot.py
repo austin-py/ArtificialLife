bf0fed6279c091deffa09df8e153e13878c839aa
austin-py <austinporras2023@u.northwestern.edu>
2023-01-18 16:04:20 -0600

    Steps 15-25

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 from sensor import SENSOR
 from motor import MOTOR
@@ -10,9 +11,11 @@ class ROBOT:
         self.sensors = {}
         self.motors = {}
         self.robotId = p.loadURDF("body.urdf")
+        self.nn = NEURAL_NETWORK("brain.nndf")
         pyrosim.Prepare_To_Simulate(self.robotId)
         self.Prepare_To_Sense()
         self.Prepare_to_Act()
+        
 
     def Prepare_To_Sense(self):
         for linkName in pyrosim.linkNamesToIndices:
@@ -29,3 +32,6 @@ class ROBOT:
     def Act(self,t):
         for motor in self.motors:
             self.motors[motor].Set_Value(self.robotId,t)
+
+    def Think(self):
+        self.nn.Print()
