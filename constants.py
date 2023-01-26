6f26924a4f3e25b0c106f6be0b12f35d571d01e3
austin-py <austinporras2023@u.northwestern.edu>
2023-01-26 15:40:19 -0600

    Task N Done

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import numpy
 
 num_steps = 1000 
 max_force = 500 
-sleep_time = 0.01
+sleep_time = 0
 
 amplitude_front = numpy.pi/4 
 frequency_front = 50
@@ -17,9 +17,11 @@ phaseOffset_back = numpy.pi/8
 targetAngles_back = numpy.linspace(0 , 2*numpy.pi, num = 1000)
 
 
-numberOfGenerations = 1
-populationSize = 1
+numberOfGenerations = 10
+populationSize = 10
 
 
 numSensorNeurons = 8 #8 
 numMotorNeurons = 7 # 7 
+
+motorJointRange = 0.5
